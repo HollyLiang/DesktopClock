2b3ceab401e00f3b88d8964fe25539710a3ad0b7
Holly Liang <holly86206@gmail.com>
2020-07-08 18:33:55 +0800

    first commit

---
+++ DesktopClock/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopClock
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Timer timer;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            setDateTime();
+
+            timer = new Timer();
+            timer.Interval = 5000;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            setDateTime();
+        }
+
+        private void setDateTime()
+        {
+            this.lblDate.Content = DateTime.Now.ToString("yyyy/M/d (dddd)");
+            this.lblClock.Content = DateTime.Now.ToString("HH:mm");
+        }
+    }
+}
